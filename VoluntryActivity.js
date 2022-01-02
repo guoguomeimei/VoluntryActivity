f0a08b77348ac90af35a47f0155db7ce977e79c4
gmq <ganmengqing0@163.com>
2022-01-01 16:59:25 -0800

    end commit

--- VoluntryActivity.js
+++ VoluntryActivity.js
@@ -1,11 +1,13 @@
 const express=require("express")
 const app=express()
 const mongoose =require('mongoose')
-const ejs =require('ejs')
+const ejs =require('ejs');
+const bodyParser = require("body-parser");
+
 
 mongoose.connect('mongodb://172.21.2.236:27017/190110910506');
 const schema1={
-    id:{type:Number,require:true},
+    id:Number,
     username:{type:String,require:true},
     password:{type:String,require:true},
     sex:String,
@@ -53,20 +55,38 @@ mongoose.connection.on('conndcted',()=>{
 })
 
 app.use('/',express.static('public'))
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
 app.use(express.json())
 app.use(express.urlencoded({extended:false})); 
-// mongoose.model('Cat',schema)
-
-// const Cat = mongoose.model('Cat', schema);
-// //model名.find({查询条件}，callback(err,查询结果)=>{}) 
-// var i=0;
-// app.use('/',express.static('public'))
-app.get("/index",(req,res,next)=>{
+//进入页面
+app.use("/index",(req,res,next)=>{
     console.log(req);
-    res.send('主页')
+    ejs.renderFile('public/index.html',function(err, str){
+        // str => 输出渲染后的 HTML 字符串
+        if(err){console.log(err)}
+        else{
+            res.setHeader('Content-Type', 'text/html');
+            res.end(str)
+        }
+    });
 })
-app.use("/activity",(req,res,next)=>{
-    ejs.renderFile('public/htmls/volactivity.html',function(err, str){
+app.use("/loginhtml",(req,res,next)=>{
+    console.log(req);
+    
+    ejs.renderFile('public/views/login.html',function(err, str){
+        // str => 输出渲染后的 HTML 字符串
+        if(err){console.log(err)}
+        else{
+            res.setHeader('Content-Type', 'text/html');
+            res.end(str)
+        }
+    });
+    
+})
+app.use("/reghtml",(req,res,next)=>{
+    console.log(req);
+    ejs.renderFile('public/views/reg.html',function(err, str){
         // str => 输出渲染后的 HTML 字符串
         if(err){console.log(err)}
         else{
@@ -76,279 +96,201 @@ app.use("/activity",(req,res,next)=>{
     });
 })
 
+var nowuser=""
+var nowpassword=""
+app.get("/login",(req,res)=>{
+    nowuser=String(req.query.username)
+    nowpassword=String(req.query.password);
+    console.log(nowuser)
+    user.findOne({ username: nowuser},  function (err, user1) {
+        if (err) return handleError(err);
+        if(nowpassword == user1.password){
+            if(user1.identity=='主办方'){
+                ejs.renderFile('public/views/manager/volorganic.html',function(err, str){
+                    // str => 输出渲染后的 HTML 字符串
+                    if(err) {
+                        console.log(err)
+                    }else{
+                        res.setHeader('Content-Type','text/html');
+                        res.end(str)
+                    }                          
+                });
+            }
+            else{
+                ejs.renderFile('public/views/activityapply.html',function(err, str){
+                    // str => 输出渲染后的 HTML 字符串
+                    if(err) {
+                        console.log(err)
+                    }else{
+                        res.setHeader('Content-Type','text/html');
+                        res.end(str)
+                    }                          
+                });
+            }
+        }
+        else{
+            console.log("your password is wrong");
+            ejs.renderFile('public/views/login.html', {info:"用户名或密码错误"},function(err, str){
+                // str => 输出渲染后的 HTML 字符串
+                if(err) {
+                    console.log(err)
+                }else{
+                    //  res.statusCode = 200;
+                    res.setHeader('Content-Type','text/html');
+                    res.end(str)
+                }
+            });
+        }
+    }
+    );
+    // next();
+});
+//注册后登录
 
+app.get("/reg",(req,res)=>{
+        reguser=req.query.username;
+        regpassword=req.query.password;
+        regsex=req.query.sex;
+        regbirth=req.querybirth;
+        reggroup=req.query.group;
+        regcardid=req.query.cardid;
+        regmajor=req.query.major;
+        regidentity=req.query.identity;
+       
+        console.log(req.query)
+        console.log(req.query.password);
+        const userreg = new user({ username: reguser , password:regpassword , sex:regsex , birth:regbirth, 
+                                        organ:reggroup, cardid:regcardid, major:regmajor, identity:regidentity});
+        userreg.save().then(()=>console.log('注册成功'))
+        ejs.renderFile('public/views/login.html', function(err, str){
+            // str => 输出渲染后的 HTML 字符串
+            if(err){console.log(err)}
+            else{
+                res.setHeader('Content-Type', 'text/html');
+                res.end(str)
+            }
+        }); 
+})
 
+app.use("/volorganic",(req,res,next)=>{
+    // console.log(req);
+    // if(nowuser==""){
+    //     ejs.renderFile('public/index.html', function(err, str){
+    //         // str => 输出渲染后的 HTML 字符串
+    //         if(err){console.log(err)}
+    //         else{
+    //             res.setHeader('Content-Type', 'text/html');
+    //             res.end(str)
+    //         }
+    //     }); 
+    // }
+    // else{
+    // user.find({username:nowuser}, function (err, use1){
+    //     if (err) return handleError(err);
+    //     if(user1.identity=="主办方"){
+            ejs.renderFile('public/views/volorganic.html', function(err, str){
+                // str => 输出渲染后的 HTML 字符串
+                if(err){console.log(err)}
+                else{
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end(str)
+                }
+            }); 
+        // }
+        // else{
+        //     ejs.renderFile('public/views/useractivity.html', function(err, str){
+        //         // str => 输出渲染后的 HTML 字符串
+        //         if(err){console.log(err)}
+        //         else{
+        //             res.setHeader('Content-Type', 'text/html');
+        //             res.end(str)
+        //         }
+        //     }); 
+            
 
-
-
-
-// public class UserService {
-
-// 	public static Connection getConnection() {
-
-// 		Connection conn = null;
-// 		try {
-// 			Class.forName("com.mysql.jdbc.Driver");
-// 			// ÊýŸÝ¿âµØÖ·
-// 			String url = "jdbc:mysql://localhost:3306/db506?useUnicode=true&characterEncoding=utf8";
-// 			String user = "root";
-// 			String pass = "123456";
-// 			conn = DriverManager.getConnection(url, user, pass);
-// 		} catch (ClassNotFoundException e) {
-// 			e.printStackTrace();
-// 			System.out.println("ÊýŸÝ¿âÇý¶¯ŽíÎó£¡");
-// 		} catch (SQLException e) {
-// 			e.printStackTrace();
-// 			System.out.print("ÊýŸÝ¿âSQLŽíÎó£¡");
-// 		}
-// 		return conn;
-// 	}
-
-// 	/**
-// 	 * žùŸÝusernameºÍpasswordœøÐÐµÇÂœ
-// 	 * @param username
-// 	 * @param password
-// 	 * @return
-// 	 */
-// 	public static boolean userLogin(String username, String password) {
-
-// 		try {
-
-// 			Connection conn = UserService.getConnection();
-
-// 			QueryRunner qr = new QueryRunner();
-
-// 			String sql = "select count(*) from t_user where username=? and password=?  ";
-
-// 			Long r = qr.query(conn, sql, new ScalarHandler<Long>(), username, password);
-
-// 			if (r > 0) {
-
-// 				return true;
-
-// 			} else {
-
-// 				return false;
-
-// 			}
-
-// 		} catch (SQLException e) {
-// 			e.printStackTrace();
-// 			System.out.print("ÊýŸÝ¿âSQLŽíÎó£¡");
-
-// 			return false;
-// 		}
-
-// 	}
-
-// 	/**
-// 	 * ×¢²áœçÃæ
-// 	 * @param username
-// 	 * @param password
-// 	 * @param sex
-// 	 * @param major
-// 	 */
-// 	public static void addUser(String username,
-
-// 			String password, String sex,Date birth, String organ, String cardid, String major, String identity) {
-// 		// TODO Auto-generated method stub
-
-// 		try {
-// 			Class.forName("com.mysql.jdbc.Driver");
-
-// 			// ÊýŸÝ¿âµØÖ·
-// 			String url = "jdbc:mysql://localhost:3306/db506?useUnicode=true&characterEncoding=utf8";
-// 			String user = "root";
-// 			String pass = "123456";
-// 			Connection conn;
-// 			conn = DriverManager.getConnection(url, user, pass);
-// 			QueryRunner qr = new QueryRunner();
-							
-// 			String sql = "insert into t_user(username,password,sex,birth,organ,cardid,major,identity) values(?,?,?,?,?,?,?,?)";
-// 			qr.update(conn, sql, username, password, sex, birth,organ, cardid, major, identity);
-
-// 		} catch (ClassNotFoundException e) {
-// 			e.printStackTrace();
-
-// 		} catch (SQLException e) {
-// 			e.printStackTrace();
-// 			System.out.println("ÊýŸÝ¿âsqlŽíÎó");
-// 		}
-
-// 	}
-	
-// 	/**
-// 	 * ²åÈëÒ»Ôò»î¶¯£¬Á¬œÓ»î¶¯×éÖ¯œçÃæ
-// 	 * žùŸÝÒ»ÏÂ²ÎÊýœøÐÐ×¢²áÒ»Ôò»î¶¯
-// 	 * @param id
-// 	 * @param name
-// 	 * @param content
-// 	 * @param starttime
-// 	 * @param endtime
-// 	 * @param time
-// 	 * @param address
-// 	 * @param capcity
-// 	 * @param host
-// 	 */
-// 	public static void addOrganic(String id,String name,
-
-// 			String content, Date starttime,Date endtime, String time, String address, String capcity, String host) {
-// 		// TODO Auto-generated method stub
-
-// 		try {
-// 			Class.forName("com.mysql.jdbc.Driver");
-
-// 			// ÊýŸÝ¿âµØÖ·
-// 			String url = "jdbc:mysql://localhost:3306/db506?useUnicode=true&characterEncoding=utf8";
-// 			String user = "root";
-// 			String pass = "123456";
-// 			Connection conn;
-// 			conn = DriverManager.getConnection(url, user, pass);
-// 			QueryRunner qr = new QueryRunner();		
-// 			String sql = "insert into t_activity(id,name,content,starttime,endtime,time,address,capcity,host) values(?,?,?,?,?,?,?,?,?)";
-// 			qr.update(conn, sql, id,name,content,starttime,endtime,time,address,capcity,host);
-
-// 		} catch (ClassNotFoundException e) {
-// 			e.printStackTrace();
-
-// 		} catch (SQLException e) {
-// 			e.printStackTrace();
-// 			System.out.println("ÊýŸÝ¿âsqlŽíÎó");
-// 		}
-
-// 	}
-
-	
-	
-// //ËùÓÐ»î¶¯µÄÐÅÏ¢ŽÓTActivtyŸ­¹ýUserServiceÒ»ÏÂº¯Êý·â×°ºó×ªµœvolactivity
-// 	/*
-// 	 * ËùÓÐÐÅÏ¢»î¶¯ºÍ±šÃûµÄÐÅÏ¢
-// 	 */
-// 	public static List<TActivity> getAllactivity() {
-
-// 		Connection conn = UserService.getConnection();
-// 		QueryRunner qr = new QueryRunner();
-
-// 		String sql = "select v.id,v.`name`,v.content,v.host,v.starttime,v.endtime,v.time,v.address,v.capcity,username from t_activity v left join t_apply p on v.`name`=p.activityname";
-		
-// 		List<TActivity> activityList = null;
-// 		try {
-// 			activityList = qr.query(conn, sql, new BeanListHandler<TActivity>(TActivity.class));
-		
-// 		} catch (SQLException e) {
-// 			e.printStackTrace();
-// 			System.out.println("ÊýŸÝ¿âSQLŽíÎó");
-
-// 		}
-// 		return activityList;
-// 	}
-	
-	
-// 	/**
-// 	 * ²éÑ°ËùÓÐµÄ»î¶¯±šÃûŒÇÂŒ£¬°üº¬ËùÓÐµÄÓÃ»§±šÃûÐÅÏ¢
-// 	 * @return
-// 	 */
-	
-	
-// 	public static List<ApplyActivity> getAllApply() {
-
-// 		Connection conn = UserService.getConnection();
-// 		QueryRunner qr = new QueryRunner();
-
-// 		String sql = "select activityname,username,regtime from t_apply";
-		
-// 		List<ApplyActivity> applyList = null;
-// 		try {
-// 			applyList = qr.query(conn, sql, new BeanListHandler<ApplyActivity>(ApplyActivity.class));
-		
-// 		} catch (SQLException e) {
-// 			e.printStackTrace();
-// 			System.out.println("ÊýŸÝ¿âSQLŽíÎó");
-
-// 		}
-// 		return applyList;
-// 	}
-	
-
-// 	/*
-// 	 * ¶ÔµÇÂœÓÃ»§²éÑ¯ÓÃ»§ÒÑŸ­±šÃûµÄ»î¶¯
-// 	 * 把用户activity中的信息
-// 	 */
-// 	public static List<UserActivity> queryUserActivity(String username) {
-
-// 		Connection conn = UserService.getConnection();
-// 		QueryRunner qr = new QueryRunner();
-
-// 		String sql = "select p.id,p.activityname,v.content,v.`host`,v.time,v.address,v.capcity,p.regtime from t_apply  p LEFT JOIN t_activity v on v.`name`=p.activityname where username=?";
-// 		List<UserActivity> useractivityList = null;
-// 		try {
-// 			useractivityList = qr.query(conn, sql, new BeanListHandler<UserActivity>(UserActivity.class),username);
-			
-// 		} catch (SQLException e) {
-// 			e.printStackTrace();
-// 			System.out.println("ÊýŸÝ¿âSQLŽíÎó");
-
-// 		}
-// 		return useractivityList;
-// 	}
-// 	/*
-// 	 * 取消报名时对申请表的操作
-// 	 */
-// 	public static void invalidUser(String activityname,String username) {
-// 		Connection conn = UserService.getConnection();
-
-// 		QueryRunner qr = new QueryRunner();
-// 		String sql = "delete from t_apply where username=? and activityname=?";
-// 		try {
-// 			qr.update(conn, sql, username,activityname);
-// 		} catch (SQLException e) {
-// 			e.printStackTrace();
-			
-// 		}
-		
-// 	}么他和哦qr = new QueryRunner();
-// 		String sql = "UPDATE t_activity SET capcity=capcity+1 where activityname=?";
-// 		try {
-// 			qr.update(conn, sql, activityname);
-// 		} catch (SQLException e) {
-// 			e.printStackTrace();
-			
-// 		}
-// 	}
-	
-// 	/*
-// 	 * 用户报名需要增加信息进入系统
-// 	 */
-// 	public static void applyUser(String activityname, String username) {
-// 		// TODO Auto-generated method stub
-// 		Connection conn = UserService.getConnection();
-
-// 		QueryRunner qr = new QueryRunner();
-// 		String sql = "INSERT INTO t_apply (activityname, username, regtime) VALUES (?, ?, NOW());";
-// 		try {
-// 			qr.update(conn, sql, activityname,username);
-// 		} catch (SQLException e) {
-// 			e.printStackTrace();
-			
-// 		}
-		
-// 	}
-
-// 	/*
-// 	 * 减少已报名数量，因为用户取消报名
-// 	 */
-// 	public static void decreaseCapcity(String activityname) {
-// 		// TODO Auto-generated method stub
-// 		Connection conn = UserService.getConnection();
-
-// 		QueryRunner qr = new QueryRunner();
-// 		String sql = "UPDATE t_activity SET capcity=capcity-1 where name=?";
-// 		try {
-// 			qr.update(conn, sql, activityname);
-// 		} catch (SQLException e) {
-// 			e.printStackTrace();
-			
-// 		}
-		
+        
+//     })
 // }
+})
+
+//查询所有活动报名信息
+app.get("/activity",(req,res)=>{
+  
+    aid=req.query.id;
+    aname=req.query.aname;
+    acontent=req.query.content;
+    aaddress=req.query.address;
+    atime=req.query.atime;
+    astarttime2=req.query.starttime2;
+    aendtime2=req.query.aendtime2;
+    atime=req.query.time
+    acapcity=req.query.capcity;
+   
+    console.log(req.query)
+    const addactivity = new activity({ id:aid,name:aname,content:acontent,addres:aaddress,time:atime,starttime:astarttime2,
+    endtime:aendtime2,time:atime,capcity:acapcity});
+    addactivity.save().then(()=>console.log('活动添加成功'))
+    ejs.renderFile('public/views/volactivity.html', function(err, str){
+        // str => 输出渲染后的 HTML 字符串
+        if(err){console.log(err)}
+        else{
+            res.setHeader('Content-Type', 'text/html');
+            res.end(str)
+        }
+    }); 
+})
+app.use("/applyActivity",(req,res,next)=>{
+    apply.find({}, function (err, apply1) {
+        ejs.renderFile('public/views/activityapply.html', {info:apply1},function(err, str){
+            // str => 输出渲染后的 HTML 字符串
+            if(err){console.log(err)}
+            else{
+                res.setHeader('Content-Type', 'text/html');
+                res.end(str)
+            }
+        });
+    })
+})
+app.use("/applyuser",(req,res,next)=>{
+    user.find({}, function (err, user1) {
+        ejs.renderFile('public/views/useractivity.html', {info:user1},function(err, str){
+            // str => 输出渲染后的 HTML 字符串
+            if(err){console.log(err)}
+            else{
+                res.setHeader('Content-Type', 'text/html');
+                res.end(str)
+            }
+        });
+    })
+})
+app.use("/userActivity",(req,res,next)=>{
+    user.find({}, function (err, user1) {
+        ejs.renderFile('public/views/useractivity.html', {info:user1},function(err, str){
+            // str => 输出渲染后的 HTML 字符串
+            if(err){console.log(err)}
+            else{
+                res.setHeader('Content-Type', 'text/html');
+                res.end(str)
+            }
+        });
+    })
+})
+
+
+// app.use("/dorsearch",(req,res,next)=>{
+//     if (searchDor==""){
+        
+app.use("/activity",(req,res,next)=>{
+    activity.find({},function(err,activity1){
+        ejs.renderFile('public/htmls/volactivity.html',{activityinfo:activity1},function(err, str){
+            // str => 输出渲染后的 HTML 字符串
+            if(err){console.log(err)}
+            else{
+                res.setHeader('Content-Type', 'text/html');
+                res.end(str)
+            }
+        });
+    })
+})
+
 app.listen(30506)
